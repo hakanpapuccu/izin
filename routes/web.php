47015f6af3462874b5aa38a05ec5a8d10b16b51f
Hakan PAPUCCU <57505363+hakanpapuccu@users.noreply.github.com>
2023-05-15 11:29:43 +0300

    Logout function complated.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
+Route::get('/dash', function () {
     return view('dashboard.content');
 });
 
@@ -31,6 +34,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::controller(UserController::class)->group(function() {
+
+    Route::get('/logout', 'destroy')->name('logout');
+
 });
 
 require __DIR__.'/auth.php';
